e68a5a49b2f9745501a294d0f01fd94cdbbef89f
natellerman <natellerman@gmail.com>
2026-04-16 15:26:10 -0400

    Added volume key control

--- IMDM290 DJ/Assets/AudioScripts/Keyboard_Input.cs
+++ IMDM290 DJ/Assets/AudioScripts/Keyboard_Input.cs
@@ -3,6 +3,9 @@ using UnityEngine.InputSystem;
 
 public class Keyboard_Input : MonoBehaviour
 {
+    public VolumeControl volumeScript;
+
+    private bool isVolumeMode = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,15 +16,36 @@ public class Keyboard_Input : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Keyboard.current.leftArrowKey.isPressed)
+        if (Keyboard.current.vKey.wasPressedThisFrame)
         {
+            isVolumeMode = !isVolumeMode;
+        }
+
+        // 2. Volume Controls
+        if (isVolumeMode)
+        {
+            if (Keyboard.current.leftArrowKey.isPressed)
+            {
+                volumeScript.AdjustVolume(-1f); // Pass -1 to go down
+            }
+            if (Keyboard.current.rightArrowKey.isPressed)
+            {
+                volumeScript.AdjustVolume(1f);  // Pass 1 to go up
+            }
+        }
+        else
+        {
+            if(Keyboard.current.leftArrowKey.isPressed)
+            {
             
+            }
+        
+            else if(Keyboard.current.rightArrowKey.isPressed)
+            {
+
+            }
         }
         
-        else if(Keyboard.current.rightArrowKey.isPressed)
-        {
-
-        }
         
     }
 }
--- IMDM290 DJ/Assets/AudioScripts/VolumeControl.cs
+++ IMDM290 DJ/Assets/AudioScripts/VolumeControl.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class VolumeControl : MonoBehaviour
 {
     public AudioClip song;
-
     public Slider volumeSlider;
-
     private AudioSource audioSource;
+
+    private bool canControlVolume = false;
+
+    public float changeSpeed = 0.5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -27,6 +30,15 @@ public class VolumeControl : MonoBehaviour
         {
             audioSource.volume = volumeSlider.value;
             //Debug.Log("Manually Changed Volume to: " + audioSource.volume);
+        }
+    }
+
+    public void AdjustVolume(float direction)
+    {
+        if (volumeSlider != null)
+        {
+            // direction will be -1 (Down) or 1 (Up)
+            volumeSlider.value += direction * changeSpeed * Time.deltaTime;
         }
     }
 
