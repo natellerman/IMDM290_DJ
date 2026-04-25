b002c5b7300072c67a1eca3933c48e43684d2ea6
MirWasHere <mirwashere@users.noreply.github.com>
2026-04-25 11:08:58 -0400

    added tutorial

--- IMDM290 DJ/Assets/AudioScripts/Keyboard_Input.cs
+++ IMDM290 DJ/Assets/AudioScripts/Keyboard_Input.cs
@@ -3,47 +3,29 @@ using UnityEngine.InputSystem;
 
 public class Keyboard_Input : MonoBehaviour
 {
-    public VolumeControl volumeScript;
-
-    private bool isVolumeMode = false;
+    [SerializeField] GameObject tutorial;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        tutorial.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Keyboard.current.vKey.wasPressedThisFrame)
+        // Showing tutorial
+        if(Keyboard.current.eKey.isPressed)
         {
-            isVolumeMode = !isVolumeMode;
-        }
+            if(tutorial.activeSelf == false)
+            {
+                tutorial.SetActive(true);
+            }
+            else
+            {
+                tutorial.SetActive(false);
+            }
 
-        // 2. Volume Controls
-        if (isVolumeMode)
-        {
-            if (Keyboard.current.leftArrowKey.isPressed)
-            {
-                volumeScript.AdjustVolume(-1f); // Pass -1 to go down
-            }
-            if (Keyboard.current.rightArrowKey.isPressed)
-            {
-                volumeScript.AdjustVolume(1f);  // Pass 1 to go up
-            }
-        }
-        else
-        {
-            if(Keyboard.current.leftArrowKey.isPressed)
-            {
-            
-            }
-        
-            else if(Keyboard.current.rightArrowKey.isPressed)
-            {
-
-            }
         }
         
         
